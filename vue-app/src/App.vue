ccaecbeafd3317b77ccff312834f5bc85c97fbc2
Reynold Figueroa <reynold0801@outlook.com>
2019-02-17 15:38:59 -0400

    .

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div v-for= "post in posts" :key="post.username">
+      <h1>Posts de {{post.username}}</h1>
+
+      <hr>
+
+      <p>{{post.post}}</p>
+
+    </div>    
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+
+export default {
+  name: 'app',
+
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  mounted() {
+    this.getpost()
+  },
+
+  methods: {
+    getpost() {
+      this.$http.get('http://localhost:3000/')
+
+      .then(res => {
+        this.posts = res.data
+        console.log(res.data)
+      })
     }
   }
 }
+</script>
+
+<style>
+
 </style>
+
